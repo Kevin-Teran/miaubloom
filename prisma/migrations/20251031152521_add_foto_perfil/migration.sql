584306e95d3bbcaea6a404ce2affb6983ad96b22
AdonadoMolinares <kevinteranmarianp@gmail.com>
2025-11-05 17:30:22 -0500

    commit

--- prisma/migrations/20251031152521_add_foto_perfil/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Perfiles_Paciente` ADD COLUMN `foto_perfil` VARCHAR(191) NULL DEFAULT '/assets/avatar-paciente.png';
-
--- AlterTable
-ALTER TABLE `Perfiles_Psicologo` ADD COLUMN `foto_perfil` VARCHAR(191) NULL DEFAULT '/assets/avatar-psicologo.png';
